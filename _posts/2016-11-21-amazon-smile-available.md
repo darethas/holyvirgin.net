e9c383261dff9a8fc22f4b676028b98bbf32500b
Dimitrios <darethas@redventures.com>
2017-02-15 15:53:36 -0500

    updates

---
+++ _posts/2016-11-21-amazon-smile-available.md
@@ -0,0 +1,11 @@
+---
+layout: news_single
+title: "Amazon Smile Donations Now Available!"
+category: news
+author: Dimitrios
+---
+You can now choose to contribute to the Church through all your Amazon purchases using our Amazon Smile link! Just follow 
+this link here: [https://smile.amazon.com/ch/58-1581579](https://smile.amazon.com/ch/58-1581579)
+
+05% of all proceeds will go to our Church as long as you use <a href="https://smile.amazon.com/ch/58-1581579">our link</a>        for your Amazon shopping needs!
+
--- gulpfile.js
+++ gulpfile.js
@@ -10,34 +10,40 @@ const sassOpts = {
 
 }
 
-gulp.task('sass', function (){
+gulp.task('sass', function () {
     return gulp.src('./_sass/main.scss')
         .pipe(sourcemaps.init())
         .pipe(sass(sassOpts).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('min', function(){
+gulp.task('min', function () {
     return gulp.src('./css/main.css')
         .pipe(cleanCSS())
         .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('rev', function() {
+gulp.task('rev', function () {
     return gulp.src('./css/main.css')
         .pipe(rev())
         .pipe(gulp.dest('./css'))
+        .pipe(rev.manifest({
+            merge: true,
+            path: 'rev_manifest.json'
+        }))
+        .pipe(gulp.dest('_data/'))
+
 });
 
 
-gulp.task('min-a-rev', function(){
+gulp.task('min-a-rev', function () {
     return gulp.src('./css/main-4ed922575d.css')
         .pipe(cleanCSS())
         .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./_sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('./_sass/**/*.scss', ['sass']);
 });
 
 gulp.task('build', ['sass', 'min', 'rev'])
